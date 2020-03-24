876d34396c9af7b17f50c9595385c03a8338a86b
Kasey Colian <kaseycolian@gmail.com>
2020-03-23 23:41:27 -0400

    about_dice_project done

--- about_dice_project.rb
+++ about_dice_project.rb
@@ -3,64 +3,81 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 # Implement a DiceSet Class here:
 #
 class DiceSet
+  attr_accessor :values
   def initialize()
-    []
+    # initialize to get new array object each time
+    @values = Array.new
   end
-  
+
+  # roll 5 dice in 1 roll & get the values
+  def roll(dice)
+    @values = [];
+    amount = dice.times
+    amount.each do |die|
+        @values << rand(1..6)
+        if (die <1) || (die > 6)
+          "value #{die} must be between 1 and 6"
+        end
+    end
+  end
 end
 
-# class AboutDiceProject < Neo::Koan
-#   def test_can_create_a_dice_set
-#     dice = DiceSet.new               
-#     assert_not_nil dice
-#   end
+class AboutDiceProject < Neo::Koan
+  def test_can_create_a_dice_set
+    dice = DiceSet.new
+    assert_not_nil dice
+  end
 
-#   def test_rolling_the_dice_returns_a_set_of_integers_between_1_and_6
-#     dice = DiceSet.new
+  def test_rolling_the_dice_returns_a_set_of_integers_between_1_and_6
+    dice = DiceSet.new
 
-#     dice.roll(5)
-#     assert dice.values.is_a?(Array), "should be an array"
-#     assert_equal 5, dice.values.size
-#     dice.values.each do |value|
-#       assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
-#     end
-#   end
+    dice.roll(5)
+    assert dice.values.is_a?(Array), "should be an array"
 
-#   def test_dice_values_do_not_change_unless_explicitly_rolled
-#     dice = DiceSet.new
-#     dice.roll(5)
-#     first_time = dice.values
-#     second_time = dice.values
-#     assert_equal first_time, second_time
-#   end
+    # .size same as .length
+    assert_equal 5, dice.values.size
+    dice.values.each do |value|
+      assert value >= 1 && value <= 6, "value #{value} must be between 1 and 6"
+    end
+  end
 
-#   def test_dice_values_should_change_between_rolls
-#     dice = DiceSet.new
+  def test_dice_values_do_not_change_unless_explicitly_rolled
+    dice = DiceSet.new
+    dice.roll(5)
+    first_time = dice.values
+    second_time = dice.values
+    assert_equal first_time, second_time
+  end
 
-#     dice.roll(5)
-#     first_time = dice.values
+  def test_dice_values_should_change_between_rolls
+    dice = DiceSet.new
 
-#     dice.roll(5)
-#     second_time = dice.values
+    dice.roll(5)
+    first_time = dice.values
 
-#     assert_not_equal first_time, second_time,
-#       "Two rolls should not be equal"
+    dice.roll(5)
+    second_time = dice.values
 
-#     # THINK ABOUT IT:
-#     #
-#     # If the rolls are random, then it is possible (although not
-#     # likely) that two consecutive rolls are equal.  What would be a
-#     # better way to test this?
-#   end
+    assert_not_equal first_time, second_time,
+      "Two rolls should not be equal"
 
-#   def test_you_can_roll_different_numbers_of_dice
-#     dice = DiceSet.new
+    # THINK ABOUT IT:
+    #
+    # If the rolls are random, then it is possible (although not
+    # likely) that two consecutive rolls are equal.  What would be a
+    # better way to test this? 
+    # by their object ids
+    assert_not_equal first_time.object_id, second_time.object_id
+  end
 
-#     dice.roll(3)
-#     assert_equal 3, dice.values.size
+  def test_you_can_roll_different_numbers_of_dice
+    dice = DiceSet.new
 
-#     dice.roll(1)
-#     assert_equal 1, dice.values.size
-#   end
+    dice.roll(3)
+    assert_equal 3, dice.values.size
 
-# end
+    dice.roll(1)
+    assert_equal 1, dice.values.size
+  end
+
+end
