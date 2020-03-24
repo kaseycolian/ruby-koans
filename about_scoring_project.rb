6dcc5da5006cc36a44ec7c37693fa75a0a963f97
Kasey Colian <kaseycolian@gmail.com>
2020-03-24 17:01:27 -0400

    All Koans completed

--- about_scoring_project.rb
+++ about_scoring_project.rb
@@ -29,37 +29,42 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 #
 # Your goal is to write the score method.
 
-def individual_dice_score(dice)
-  dice.each do | d |
-    if (d == 1)
-      @score += 100
-    elsif (d == 5)
-      @score += 50 
-    end
-  end
+def score(dice)
+  @score = 0
+  check_for_triples(dice)
+  individual_dice_score(dice)
   @score
 end
 
-def digit_count(digit)
-  @dice.select { |d| digit == d }.count
+def individual_dice_score(dice)
+  dice.each do | d |
+    if d == 1
+      @score += 100
+    elsif d == 5
+      @score += 50
+    end
+  end
 end
 
-def score(dice)
-  @count = 0
-  @score = 0
+def check_for_triples(dice)
+  singles = dice.uniq
 
-  dice.each { |d| @count +=1 }
- 
-
-  if(@count == 0)
-    return @score
-  end
-
-  if (@count >= 3)
-
-    return @score
-  else
-    individual_dice_score(dice)
+  singles.each do | n |
+    count = 0
+    dice.each do |d|
+      if d == n
+        count += 1
+        if (count > 0 ) && (count % 3 == 0)
+          if n == 1
+            3.times { dice.delete_at(dice.index(d)) }
+            @score += 1000
+          else 
+            3.times { dice.delete_at(dice.index(d)) }
+            @score = 100 * n
+          end
+        end    
+      end
+    end
   end
 end
 
@@ -76,32 +81,32 @@ class AboutScoringProject < Neo::Koan
     assert_equal 100, score([1])
   end
 
-  # def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
-  #   assert_equal 300, score([1,5,5,1])
-  # end
+  def test_score_of_multiple_1s_and_5s_is_the_sum_of_individual_scores
+    assert_equal 300, score([1,5,5,1])
+  end
 
-  # def test_score_of_single_2s_3s_4s_and_6s_are_zero
-  #   assert_equal 0, score([2,3,4,6])
-  # end
+  def test_score_of_single_2s_3s_4s_and_6s_are_zero
+    assert_equal 0, score([2,3,4,6])
+  end
 
-  # def test_score_of_a_triple_1_is_1000
-  #   assert_equal 1000, score([1,1,1])
-  # end
+  def test_score_of_a_triple_1_is_1000
+    assert_equal 1000, score([1,1,1])
+  end
 
-  # def test_score_of_other_triples_is_100x
-  #   assert_equal 200, score([2,2,2])
-  #   assert_equal 300, score([3,3,3])
-  #   assert_equal 400, score([4,4,4])
-  #   assert_equal 500, score([5,5,5])
-  #   assert_equal 600, score([6,6,6])
-  # end
+  def test_score_of_other_triples_is_100x
+    assert_equal 200, score([2,2,2])
+    assert_equal 300, score([3,3,3])
+    assert_equal 400, score([4,4,4])
+    assert_equal 500, score([5,5,5])
+    assert_equal 600, score([6,6,6])
+  end
 
-  # def test_score_of_mixed_is_sum
-  #   assert_equal 250, score([2,5,2,2,3])
-  #   assert_equal 550, score([5,5,5,5])
-  #   assert_equal 1100, score([1,1,1,1])
-  #   assert_equal 1200, score([1,1,1,1,1])
-  #   assert_equal 1150, score([1,1,1,5,1])
-  # end
+  def test_score_of_mixed_is_sum
+    assert_equal 250, score([2,5,2,2,3])
+    assert_equal 550, score([5,5,5,5])
+    assert_equal 1100, score([1,1,1,1])
+    assert_equal 1200, score([1,1,1,1,1])
+    assert_equal 1150, score([1,1,1,5,1])
+  end
 
 end
